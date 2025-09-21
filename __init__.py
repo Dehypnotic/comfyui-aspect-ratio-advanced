8ef615353f354ed3fca1bcab39ef6463ecfefc02
dehypnotic <jegrmeg@outlook.com>
2025-09-21 14:50:05 +0200

    Endret init

--- __init__.py
+++ __init__.py
@@ -1,7 +1,1 @@
 from .aspect_ratio_advanced import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-
-# Clean display name for UI
-try:
-    NODE_DISPLAY_NAME_MAPPINGS["AspectRatioAdvanced"] = "Aspect Ratio Advanced"
-except Exception:
-    pass
